b7fa4ae91f286a7048968d024d819fd6a14da0fc
Zhivko Shopov <zhishopov@gmail.com>
2025-07-21 13:41:28 +0100

    First component

--- my-first-app/src/app/app.ts
+++ my-first-app/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Hello } from './hello/hello';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Hello],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('my-first-app');
