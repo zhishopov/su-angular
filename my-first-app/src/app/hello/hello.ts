c16e429d9ce3f7249611f26dccc643d8aab1f6c1
Zhivko Shopov <zhishopov@gmail.com>
2025-07-28 14:37:14 +0100

    Rendering an image

--- my-first-app/src/app/hello/hello.ts
+++ my-first-app/src/app/hello/hello.ts
@@ -11,6 +11,8 @@ import { Company } from '../models/company';
 export class Hello {
   name: string = 'Stefan';
   isLoggedIn: boolean = false;
+  img: string =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFUAfyVe3Easiycyh3isP9wDQTYuSmGPsPQvLIJdEYvQ_DsFq5Ez2Nh_QjiS3oZ3B8ZPfK9cZQyIStmQMV1lDPLw';
 
   users: User[] = [
     {
@@ -40,7 +42,7 @@ export class Hello {
     console.log('Initializing Component');
   }
 
-  onGreet(name: string) {
-    console.log(`Hello ${name}`);
+  onGreet($event: any) {
+    alert('Hello');
   }
 }
